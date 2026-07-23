610a90dfcc92f543face8a6ac472473c985c6fae
Marcel Jung <MarcelJung97@web.de>
2026-07-23 17:10:08 +0200

    feat: add the FPGA emulation files for the macro counter example

---
+++ macros/heichips26_digital_project/macros/counter/fpga/dut.mk
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2026 The HeiChips contributors
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+
+# RTL of the macro under test, shared by all boards.
+
+SRC_DIR := ../../rtl
+
+DUT_SRCS := \
+	$(SRC_DIR)/counter.sv
